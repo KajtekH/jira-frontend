3f0a9543afe395580a41dabe5857cb1f49d22e51
KajtekH <kajhulboj@wp.pl>
2025-04-14 21:54:07 +0200

    add register form

--- src/app/components/login-component/login.component.ts
+++ src/app/components/login-component/login.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import * as CryptoJS from 'crypto-js';
 import {
   ButtonBarComponent, ButtonComponent,
@@ -16,6 +16,9 @@ import {CdkDrag} from "@angular/cdk/drag-drop";
 import {LoginRequest} from "../../models/auth/login-request";
 import {AuthService} from "../../services/auth-services/auth.service";
 import {Router} from "@angular/router";
+import {TaskRequest} from "../../models/task/taskRequest.interface";
+import {DialogService} from "@fundamental-ngx/core/dialog";
+import {RegisterRequest} from "../../models/auth/register-request";
 
 @Component({
   selector: 'app-login-component',
@@ -28,31 +31,51 @@ import {Router} from "@angular/router";
     FormLabelComponent,
     ReactiveFormsModule,
     ButtonComponent,
-    ToolbarItemDirective
+    ToolbarItemDirective,
+    ButtonBarComponent,
+    CdkDrag,
+    DialogCloseButtonComponent,
+    DialogComponent,
+    DialogHeaderComponent,
+    TitleComponent,
+    DialogTemplateDirective
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit {
-  myForm!: FormGroup;
+  loginForm!: FormGroup;
+  registerForm!: FormGroup;
+  @ViewChild('overlay')
+  overlay: ElementRef<HTMLElement> | undefined;
 
   constructor(private authService: AuthService,
               private _fb: FormBuilder,
-              private router: Router) {
+              private _fb2: FormBuilder,
+              private router: Router,
+              private _dialogService: DialogService) {
   }
 
   ngOnInit(): void {
-    this.myForm = this._fb.group({
+    this.loginForm = this._fb.group({
       usernameInput: new FormControl(''),
       passwordInput: new FormControl(''),
+    });
+    this.registerForm = this._fb2.group({
+      registerUsernameInput: new FormControl(''),
+      registerPasswordInput: new FormControl(''),
+      emailInput: new FormControl(''),
+      confirmPasswordInput: new FormControl(''),
+      firstNameInput: new FormControl(''),
+      lastNameInput: new FormControl(''),
     });
     this.authService.stopTokenRefresh();
   }
 
   onLogin() {
     const loginRequest: LoginRequest = {
-      email: this.myForm.get('usernameInput')?.value,
-      password: this.myForm.get('passwordInput')?.value
+      email: this.loginForm.get('usernameInput')?.value,
+      password: this.loginForm.get('passwordInput')?.value
     }
     this.authService.login(loginRequest).subscribe(
       (response) => {
@@ -69,5 +92,31 @@ export class LoginComponent implements OnInit {
         console.error('Login failed:', error);
       }
     );
+  }
+
+  openDialog(dialog: TemplateRef<any>): void {
+    const dialogRef = this._dialogService.open(dialog, { responsivePadding: false, resizable: false });
+
+    dialogRef.afterClosed.subscribe((result) => {
+      const registerRequest: RegisterRequest = {
+        email: this.registerForm.get('emailInput')?.value,
+        username: this.registerForm.get('registerUsernameInput')?.value,
+        password: this.registerForm.get('registerPasswordInput')?.value,
+        firstName: this.registerForm.get('firstNameInput')?.value,
+        lastName: this.registerForm.get('lastNameInput')?.value
+      };
+      console.log(registerRequest);
+      if (registerRequest.password  !== this.registerForm.get('confirmPasswordInput')?.value) {
+        console.log(registerRequest.password);
+        console.log(this.registerForm.get('confirmPasswordInput')?.value);
+        console.log('Passwords do not match');
+        return;
+      }
+      this.authService.register(registerRequest).subscribe((response) => {
+        console.log('Registration successful:', response);
+      });
+    }, (error) => {
+      console.error('Dialog closed with error:', error);
+    });
   }
 }
